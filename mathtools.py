d6a037fa607681a3116d247abf09d1b0c0cecf76
vvaltanen <valtanen.v@gmail.com>
2017-10-06 11:38:53 +0300

    added serie sum arithmetics

--- mathtools.py
+++ mathtools.py
@@ -16,3 +16,12 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+		
+def arithmetic(a, difference, n):
+    '''Calculates the sum of a arithmetic serie of n elements.
+       An arithmetic sequence is of the form: a, a+d, a+2d, a+3d,...
+       n is the number of elements in the sequence.'''
+    #Get the arithmetic sequence
+    sequence = [a+difference*x for x in range(n)]
+    #Calculates its sum
+    return sum(sequence)
