de9e13f4ad6d98483f01a4d9e829d549ee20c67d
vvaltanen <valtanen.v@gmail.com>
2017-10-06 11:40:49 +0300

    added serie sum arithmetics

--- mathtools.py
+++ mathtools.py
@@ -26,3 +26,12 @@ def fib(n):
         return 1
     else:
         return fib(n-1)+fib(n-2)
+		
+def arithmetic(a, difference, n):
+    '''Calculates the sum of a arithmetic serie of n elements.
+       An arithmetic sequence is of the form: a, a+d, a+2d, a+3d,...
+       n is the number of elements in the sequence.'''
+    #Get the arithmetic sequence
+    sequence = [a+difference*x for x in range(n)]
+    #Calculates its sum
+    return sum(sequence)
