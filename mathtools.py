e91ce4ac638403227263956c34020e84d18c89b5
vvaltanen <valtanen.v@gmail.com>
2017-10-06 12:03:09 +0300

    alice

--- mathtools.py
+++ mathtools.py
@@ -36,3 +36,12 @@ def arithmetic(a, difference, n):
     sequence = [a+difference*x for x in range(n)]
     #Calculates its sum
     return sum(sequence)
+	
+def geometric(a, ratio, n):
+    '''Calculates the sum of a geometric serie of n elements.
+       A geometric sequence is of the form: a, a*r, a*r*r, a*r*r*r,...
+       n is the number of elements in the sequence.'''
+    #Get the geometric sequence
+    sequence = [a*(ratio**x) for x in range(n)]
+    #Calculates its sum
+    return sum(sequence)
